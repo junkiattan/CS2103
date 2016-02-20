46dec37590d0f68e56c00c862f083ad60ead2ac0
junkiattan <cyan4936@gmail.com>
2016-02-20 21:21:53 +0800

    Modification across all methods
    1. separate scanner and printwriter classes
    2. modification of methods to ensure that  they all return strings and
    printing is done at higher levels
    3. arguements for each method is further simplified

--- TextBuddy.java
+++ TextBuddy.java
@@ -9,6 +9,12 @@ import java.io.PrintWriter;
 // 2. To delete, create a new instance of PrintWriter to bring cursor back up. Using print instead of println will do
 // 3. Each time print or println is used, you must flush it immediately afterwards
 
+// 4. In the implementation, separate the printing from the functionalities of the methods, dont combine them together.
+//Save the printing for the higher levels, e.g. modifyInput or in main instead of in each sub-method themselves such as add, etc.
+// 5. If the scanner needs to be used throughout the program, instead of declaring it as global, can use the Singleton pattern for Scanner
+// 6. Unlike for Scanner in which one instance can be reused, multiple instances of PrintWriter must be created, hence dont need to create
+//a separate method for creating the printWriter based on the fileName
+
 /**
  * 
  * @author Tan Jun Kiat
@@ -28,13 +34,12 @@ class IO{
 	public static String getNextLine(){
 		return getSystemScanner().nextLine();
 	}
-	
-	
-	//public static PrintWriter get
 }
+
 
 public class TextBuddy {
 	
+	// All strings to have newline characters to standardise for use in mainMessagePrinter method
 	private static final String MESSAGE_WELCOME = "Welcome to TextBuddy. %1$s is ready for use\n";
 	private static final String MESSAGE_DISPLAY = "%1$s. %2$s\n";
 	private static final String MESSAGE_DELETE = "deleted from %1$s: \"%2$s\"\n";
@@ -43,131 +48,156 @@ public class TextBuddy {
 	private static final String MESSAGE_CLEAR = "all content deleted from %1$s\n";
 	private static final String MESSAGE_INVALID = "Invalid command\n";
 	private static final String MESSAGE_INVALID_DELETE = "Item does not exist or has already been deleted\n";
-	private static ArrayList<String> stringStorage;
+	//private static final String MESSAGE_INVALID_SORT = "List cannot be sorted\n";
+	
+	private static ArrayList<String> stringStorage = new ArrayList<String>();
 	
 	public static void main(String args[]) throws FileNotFoundException{ // args[0] is the filename
-		mainMessagePrinter(welcomeMessage(args));
 		
-		stringStorage = new ArrayList<String>();
-		
-		File userFile = new File(args[0]);
-		PrintWriter printWriter = null;
-		try {
-			printWriter = new PrintWriter(userFile);
-		} catch (FileNotFoundException e) {}
-		
-		while (true){	
-			modifyInput(args, userFile, printWriter);
+		mainMessagePrinter(welcomeMessage(args[0]));
+		while (true){
+			executeCommand(args[0]);
 		}
 	}
 	
+	// overload the method to print for display
 	public static void mainMessagePrinter(String sentence){
 		System.out.print(sentence);
 	}
-
-	public static String welcomeMessage(String[] args){
-		String printSentence = String.format(MESSAGE_WELCOME, args[0]);
-		return printSentence;
+	public static void mainMessagePrinter(ArrayList<String> storage){
+		for (String s: storage){
+			System.out.print(s);
+		}
 	}
 
-	private static void modifyInput(String[] args, File userFile,
-			PrintWriter printWriter) throws FileNotFoundException {
+	public static String welcomeMessage(String fileName){
+		String printSentence = String.format(MESSAGE_WELCOME, fileName);
+		return printSentence;
+	}
+	
+	// Following 2 methods to get file and create a print writer based on the fileName
+	public static File getFile(String fileName){
+		File file = null;
+		try {
+			file = new File(fileName);
+		} catch (Exception e){
+		}
+		return file;
+	}
+	
+	public static PrintWriter getPrintWriter(String fileName){
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(getFile(fileName));
+		} catch (Exception e) {
+		}
+		return pw;
+	}
+	
+	// main executeCommand functions. Printing for the lower level methods is done here before it goes back up to user
+	public static void executeCommand(String fileName){
 		String[] input = IO.getNextLine().split(" ");
 		switch (input[0])
 		{
 		case "add":
-			addAndPrint(args, stringStorage, printWriter, input);
+			mainMessagePrinter(add(fileName, input));
 			break;
 		case "display":
-			displayAndPrint(args, stringStorage);
+			mainMessagePrinter(display(fileName));
 			break;
 		case "delete":
-			deleteAndPrint(args, stringStorage, userFile, input);
+			mainMessagePrinter(delete(fileName, input));
 			break;
 		case "clear":
-			clearAndPrint(args, stringStorage, userFile);
+			mainMessagePrinter(clear(fileName));
 			break;
 		case "exit":
 			System.exit(0);
 		default:
 			//throw an error if the command is not recognized
-			System.out.printf(MESSAGE_INVALID);
+			mainMessagePrinter(MESSAGE_INVALID);
 		}
 	}
+	
+	// Pre: String fileName specifies the desired file to be written into while String[] input represents the data
+	//to be written
+	// Post: data to be written into file, feedback in form of String is returned
+	public static String add(String fileName, String[] input) {
+		// adds the text to the file
+		String sentence = convertToString(input);
+		stringStorage.add(sentence);
+		fileEditor(fileName, sentence);
+		return String.format(MESSAGE_ADD, fileName, sentence);
+	}
 
-	public static String displayAndPrint(String[] args, ArrayList<String> stringStorage) {
+	// Pre: String fileName specifies the file to be read
+	// Post: stores each item in the file (stored in stringStorage for easy retrieval) inside an arrayList and returns that arrayList for printing later
+	public static ArrayList<String> display(String fileName) {
+		ArrayList<String> displayStorage = new ArrayList<String>();
 		// if there are text to be displayed
 		// displayPrintFunction to print out text in order of addition
 		if (!stringStorage.isEmpty()){
 			for (int i=0; i<stringStorage.size(); i++){
-				String printSentence = String.format(MESSAGE_DISPLAY, (i+1), stringStorage.get(i));
-				System.out.print(printSentence);
-				return printSentence;
-				//System.out.printf(MESSAGE_DISPLAY, (i+1), stringStorage.get(i));
+				displayStorage.add(String.format(MESSAGE_DISPLAY, (i+1), stringStorage.get(i)));
 			}
 		} else {
 		// if file is empty, to display print below
-		String printSentence = String.format(MESSAGE_EMPTY, args[0]);
-		System.out.print(printSentence);
-		return printSentence;
-		//System.out.printf(MESSAGE_EMPTY, args[0]);
+		// single String sentence is added to arrayList for consistency
+		displayStorage.add(String.format(MESSAGE_EMPTY, fileName));
 		}
-		return null;
+		return displayStorage;
 	}
-
-	private static void addAndPrint(String[] args, ArrayList<String> stringStorage, PrintWriter printWriter,
-			String[] input) {
-		// adds the text to the file
-		String sentence = convertToString(input);
-		stringStorage.add(sentence);
-		fileAdder(printWriter, sentence);
-		System.out.printf(MESSAGE_ADD, args[0], sentence);
-	}
-
-	private static void fileAdder(PrintWriter printWriter, String sentence) {
-		printWriter.println(sentence);
-		printWriter.flush();
-	}
-	private static void fileDeleter(PrintWriter printWriter, String sentence) {
-		printWriter.print(sentence);
-		printWriter.flush();
-	}
-
-	private static void clearAndPrint(String[] args, ArrayList<String> stringStorage, File userFile)
-			throws FileNotFoundException {
-		PrintWriter clearPrintWriter = new PrintWriter(userFile);
-		// clear the content from the text file
-		// after which add below print statement
-		stringStorage.clear();
-		fileDeleter(clearPrintWriter, "");
-		clearPrintWriter.close();
-		System.out.printf(MESSAGE_CLEAR, args[0]);
-	}
-
-	private static void deleteAndPrint(String[] args, ArrayList<String> stringStorage, File userFile, String[] input)
-			throws FileNotFoundException {
+	
+	// Pre: String fileName of desired file to be deleted from and the desired id of the item to be deleted (stored in input)
+	// Post: Returns String feedback depending on whether the operation is successful
+	public static String delete(String fileName, String[] input) {
 		int numOfRemovable = -1;
 		try{
 			numOfRemovable = Integer.parseInt(input[1]);
 		} catch (Exception e){}
 		if (numOfRemovable == 0 || numOfRemovable > stringStorage.size()){
-			System.out.println(MESSAGE_INVALID_DELETE);
+			return MESSAGE_INVALID_DELETE;
 		} else if (numOfRemovable == -1){
-			System.out.println(MESSAGE_INVALID);
+			return MESSAGE_INVALID;
 		} else {
 			String word = stringStorage.remove(numOfRemovable-1);
 			// use the numOfRemovable to remove desired text within file
-			PrintWriter deleterPrintWriter = new PrintWriter(userFile);
-			fileDeleter(deleterPrintWriter, "");
+			fileEditor(fileName, "");
 			for (String s: stringStorage){
-				fileAdder(deleterPrintWriter, s);
+				fileEditor(fileName, s);
 			}
-			// after which close and follow up with feedback
-			deleterPrintWriter.close();
-			System.out.printf(MESSAGE_DELETE, args[0], word);
+			// after which follow up with feedback
+			return String.format(MESSAGE_DELETE, fileName, word);
 		}
 	}
 
+	//Pre: Strind fileName of the desired file to be cleared
+	// Post: Returns String feedback to indicate successful clearing of desired file
+	public static String clear(String fileName) {
+		// clear the content from the text file
+		// after which add below print statement
+		stringStorage.clear();
+		fileEditor(fileName, "");
+		return String.format(MESSAGE_CLEAR, fileName);
+	}
+	
+	// Pre: desired fileName
+	// Post: Method to create a separate instance of PrintWriter each time it is called,
+	// in addition the sentence is written into specific file
+	public static void fileEditor(String fileName, String sentence) {
+		PrintWriter pw = getPrintWriter(fileName);
+		if (sentence.equals("")){
+			// print is meant for the delete method where the printWriter prints and empty string ""
+			pw.print(sentence);
+		} else {
+			// println is meant for the non-delete methods like add where printWriter writes in a specific string sentence
+			pw.println(sentence);
+		}
+		pw.flush();
+	}
+
+	//Pre: String array containing words to be concatenated
+	//Post: String representing the concatenated words in the array
 	public static String convertToString(String[] input){
 		String sentence = "";
 		for (int i=1; i<input.length; i++){
@@ -178,6 +208,11 @@ public class TextBuddy {
 			}
 		}
 		return sentence;
+	}
+	
+	//
+	public static void sortLines(String fileName){
+		
 	}
 	
 }
